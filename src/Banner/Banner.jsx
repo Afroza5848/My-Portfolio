0525817c46b525cb316276b25418507d5c0653e0
Afroza5848 <suriyaafroj5848@gmail.com>
2024-07-03 19:41:15 +0600

    portfolio initialized

---
+++ src/Banner/Banner.jsx
@@ -0,0 +1,53 @@
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import bannerImg from '../assets/image/me.jpg'
+import { IoDownloadSharp } from "react-icons/io5";
+
+const Banner = () => {
+    const [typeEffect] = useTypewriter({
+        words: [' React Frontend Developer','Web Developer','UI Developer'],
+        loop: {},
+        typeSpeed: 300,
+        delaySpeed: 50,
+    });
+    return (
+        <div
+            className="hero min-h-screen"
+            style={{
+                backgroundImage: `url(${bannerImg})`,
+            }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="text-neutral-content text-center ">
+                <div className="">
+                    <h1 className="mb-6 text-5xl">Hello I,am <span className='font-bold roboto'>Afroza <span className='bg-gradient-to-r from-cyan-400 via-pink-400 to-cyan-400 inline-block text-transparent bg-clip-text'>Khatun</span></span></h1>
+
+                    <p className='mb-6 text-2xl text-pink-300 font-semibold'>{typeEffect}  <Cursor cursorColor='cyan' /></p>
+                    
+                    {/* <div className=''>
+                        <button className="btn roboto text-xl">Resume <IoDownloadSharp className='mb-1' /></button>
+                    </div> */}
+                    <div className='space-x-4'>
+                        <a href="https://drive.google.com/file/d/1OhN79epCtxO4Z6qbOMzDTD60-uOCMf1z/view?usp=sharing" target='_blank' className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border border-cyan-500 rounded-md shadow-md group">
+                            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-xl text-white duration-300 -translate-x-full bg-cyan-500 group-hover:translate-x-0 ease">
+                                <IoDownloadSharp className='' />
+                            </span>
+                            <span className="absolute flex items-center justify-center w-full h-full text-cyan-500 transition-all duration-300 transform group-hover:translate-x-full text-xl roboto ease">Resume</span>
+                            <span className="relative invisible">Resume</span>
+                        </a>
+
+
+                        <a href="#_" className="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+                            <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-red-500 rounded-full blur-md ease"></span>
+                            <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+                                <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-cyan-500 rounded-full blur-md"></span>
+                                <span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+                            </span>
+                            <span className="relative text-white text-xl roboto">Contact</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const Navbar = () => {
+    return (
+        <div className="max-w-7xl mx-auto py-5 fixed top-0 left-0 right-0">
+            <div className="navbar ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a className="text-xl font-semibold roboto">Home</a></li>
+                            <li><a className="text-xl font-semibold roboto">About</a></li>
+                            <li><a className="text-xl font-semibold roboto">Skills</a></li>
+                            <li><a className="text-xl font-semibold roboto">Projects</a></li>
+                            <li><a className="text-xl font-semibold roboto">Contact</a></li>
+
+                        </ul>
+                    </div>
+                    <a className=" text-4xl text-white font-bold">Afroza<span className="bg-gradient-to-r from-cyan-400 via-pink-400 to-cyan-400 inline-block text-transparent bg-clip-text roboto">Khatun</span></a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a className="text-xl font-medium text-white roboto">Home</a></li>
+                        <li><a className="text-xl font-medium text-white roboto">About</a></li>
+                        <li><a className="text-xl font-medium text-white roboto">Skills</a></li>
+                        <li><a className="text-xl font-medium text-white roboto">Projects</a></li>
+                        <li><a className="text-xl font-medium text-white roboto">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end space-x-3">
+                    <a href="https://www.linkedin.com/in/afroza5848/" className="px-5 py-2 bg-cyan-500 rounded"><FaLinkedinIn></FaLinkedinIn></a>
+                    <a href="https://github.com/Afroza5848" className="px-5 py-2 bg-cyan-500 rounded"><FaGithub></FaGithub></a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
